fc754d0d3c5983fce69f0ae1ce86bc5d63265046
francia5 <francia.va.or@gmail.com>
2018-07-30 20:57:30 -0500

    se agergan modificaciones

--- js/index.js
+++ js/index.js
@@ -9,19 +9,49 @@ var config = {
 };
 firebase.initializeApp(config);
 
-function login (click) {
-    var btn = document.getElementById('login');
-    btn.addEventListener('click');
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-    });
+// function login (click) {
+//     var btn = document.getElementById('login');
+//     btn.addEventListener('click');
+//     firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+//         // Handle Errors here.
+//         var errorCode = error.code;
+//         var errorMessage = error.message;
+//         // ...
+//     });
 
-    firebase.auth().signOut().then(function () {
-        // Sign-out successful.
-    }).catch(function (error) {
-        // An error happened.
-    });
-}
+//     firebase.auth().signOut().then(function () {
+//         // Sign-out successful.
+//     }).catch(function (error) {
+//         // An error happened.
+//     });
+// }
+
+$(document).ready(() => {
+    $("#signup").click(() => {
+        let email = $("#email").val();
+        let password = $("#password").val();
+
+        if (password == 123456) {
+            swal("Error ", " The password can not be 123456, empty field or have less than 6 characters", "error");
+            let passwordInput = $("#password").val("");
+        } else {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+                .catch((error) => {
+                    alert("the email or the password is incorrect");
+                })
+        }
+
+
+
+    })       
+
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            $("#user").text(user.email);
+            window.location = "app.html";
+        } else {
+            $("#user").text("");
+        }
+    })
+
+});
